ed3e602152ef2bd2ac44084da2dd46e19717c617
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-04-19 08:28:25 -0700

    add Food class

---
+++ SnakeGameUsingConsoleGameEngine/Food.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameEngine;
+
+namespace SnakeGameUsingConsoleGameEngine
+{
+    public class Food : GameObject
+    {
+
+    }
+}
